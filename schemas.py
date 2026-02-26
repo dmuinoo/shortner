be2c7897a558ea4b84b8aea7c913cc961576fb4d
Diego <dmuinoo@gmail.com>
2026-02-26 22:38:00 +0100

    Primer commit, Ejercicio de Acrtador de URLs funcionando correctamente

---
+++ schemas.py
@@ -0,0 +1,20 @@
+# short/schemas.py
+
+
+from pydantic import BaseModel, ConfigDict
+
+class URLBase(BaseModel):
+	target_url: str
+
+class URL(URLBase):
+	is_active: bool
+	clicks: int
+
+	model_config = ConfigDict(from_attributes=True)
+
+
+class URLInfo(URL):
+	url: str
+	admin_url: str
+
+	model_config = ConfigDict(from_attributes=True)
